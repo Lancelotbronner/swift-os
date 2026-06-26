7219f1d27928ef3ad4eb2e95f0ec723e05e818ee
Christophe Bronner <christophe.bronner@videotron.ca>
2026-06-26 13:38:48 -0400

    Squashed commit of the following:
    commit 62ac98a4d82ee7f18da83e4ba5c4534c40081b7c
    Author: Christophe Bronner <christophe.bronner@videotron.ca>
    Date:   Fri Jun 26 13:38:12 2026 -0400
    
        move the FIXME to the new problematic location
    
    commit 8c9dbd0f96d27baf96ff613f40b49669f86c5d49
    Author: Christophe Bronner <christophe.bronner@videotron.ca>
    Date:   Fri Jun 26 10:02:29 2026 -0400
    
        fixes and clarity changes
    
    commit 8eeae11fe1bec9ea67c085822577f0840717aeb5
    Author: Christophe Bronner <christophe.bronner@videotron.ca>
    Date:   Fri Jun 26 09:03:45 2026 -0400
    
        Almost works
    
    commit 714053e262d1c835190c637d1e0b6f8512836674
    Author: Christophe Bronner <christophe.bronner@videotron.ca>
    Date:   Fri Jun 26 07:27:21 2026 -0400
    
        Switch to booting from Swift

--- Sources/RaspberryPi/RaspberryPi.swift
+++ Sources/RaspberryPi/RaspberryPi.swift
@@ -4,19 +4,19 @@ import EmbeddedArch
 public struct RaspberryPi {
 	public init() {}
 
-	public func main() {
+	public static func main() {
 		zeroBSS()
 
-		#if RASPI
-			let _ = UARTConsole(uart: UART0())
-		#else
-			// let console = OtherConsole()
-			fatalError("not implemented")
-		#endif
+#if RASPI
+		let _ = UARTConsole(uart: UART0())
+#else
+		// let console = OtherConsole()
+		fatalError("not implemented")
+#endif
 
-		#if arch(arm64)
-			register_vector_table()
-		#endif
+#if arch(arm64)
+		register_vector_table()
+#endif
 
 		// Initialise the GIC-400 interrupt controller before enabling
 		// any device interrupts.
@@ -32,31 +32,31 @@ public struct RaspberryPi {
 		print(memoryManager.total / 1024 / 1024, terminator: " ")
 		print("MiB")
 
-		#if RASPI
-			let fb = RPiFramebuffer<UInt32>(width: 1920, height: 1080, pixelOrder: .rgb)
-		#else
-			// let fb = OtherFramebuffer()
-			fatalError("not implemented")
-		#endif
+#if RASPI
+		let fb = RPiFramebuffer<UInt32>(width: 1920, height: 1080, pixelOrder: .rgb)
+#else
+		// let fb = OtherFramebuffer()
+		fatalError("not implemented")
+#endif
 		var g = Graphics(target: fb)
 		g.fillRect(x0: 0, y0: 0, x1: 100, y1: 100, color: 0xffffff)
 		g.drawString("Hello Swift!", x: 0, y: 100, color: 0xffffff)
 
-		#if arch(arm64)
-			// For debugging
-			brk0()
+#if arch(arm64)
+		// For debugging
+		brk0()
 
-		let el = get_el().EL
-			let elLabel: StaticString = "Exception Level:"
-			print(elLabel, terminator: " ")
-			print(el)
-			#if RASPI
-				g.drawString(elLabel, x: 0, y: 108, color: 0xffffff)
-				for i in 0..<Int(el) {
-					g.drawString("I", x: (elLabel.utf8CodeUnitCount + i) * 8, y: 108, color: 0xffffff)
-				}
-			#endif
-		#endif
+		let elr = get_CurrentEL()
+		let elLabel: StaticString = "Exception Level:"
+		print(elLabel, terminator: " ")
+		print(elr.el)
+#if RASPI
+		g.drawString(elLabel, x: 0, y: 108, color: 0xffffff)
+		for i in 0..<Int(elr.el) {
+			g.drawString("I", x: (elLabel.utf8CodeUnitCount + i) * 8, y: 108, color: 0xffffff)
+		}
+#endif
+#endif
 
 		repeat { halt() } while true
 	}
